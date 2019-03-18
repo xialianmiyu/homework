a96064a67f5c703268240f6ab4818fecb1a2e600
xialianmiyu <284052169@qq.com>
2019-03-18 23:22:44 +0800

    factory

---
+++ pattern/src/main/java/simple/Computer.java
@@ -0,0 +1,5 @@
+package simple;
+
+public abstract class  Computer {
+    abstract String getBrand();
+}
---
+++ pattern/src/main/java/simple/Lenovo.java
@@ -0,0 +1,8 @@
+package simple;
+
+public class Lenovo extends Computer {
+
+    public String getBrand() {
+        return "联想";
+    }
+}
